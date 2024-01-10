4529159900652d2b2930aedb5cc52b26d4ba3aba
harshitverma2103 <harshitverma2103@gmail.com>
2024-01-10 13:25:56 +0530

    add logic to add value , remove value and back to normal state zero

--- counter_project/src/App.jsx
+++ counter_project/src/App.jsx
@@ -1,33 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+let [counter , setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1)
+    if(counter  >= 20) {
+      alert('Oops! no you can not add value more than 20')
+      setCounter(counter = 20)
+    }
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+    if(counter < 1 ) {
+      alert('Oops! no you can not value count less than 0')
+      setCounter(counter  = 0)
+    }
+  }
+
+  const backToZero = () => {
+    if(counter == 0) {
+      alert('Oops! already 0')
+    }
+    setCounter(counter = 0)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chlo count Kare! </h1>  
+      <h2>counter value is {counter}</h2>
+      <button 
+      onClick={addValue}
+      >Add Value</button>
+      <br /> 
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+      <br />
+      <br />
+      <button 
+      onClick={backToZero}
+      >Back to 0</button>      
     </>
   )
 }
